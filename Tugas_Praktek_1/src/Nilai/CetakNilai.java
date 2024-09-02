e273dde9e07cb8af77bf85762cc9ddc5940527ed
k7laundry <desmandwisaputra@gmail.com>
2024-09-02 23:19:02 +0700

    commited

---
+++ Tugas_Praktek_1/src/Nilai/CetakNilai.java
@@ -0,0 +1,14 @@
+package Nilai;
+public class CetakNilai {
+    public static void main(String[] args) {
+        Nilai mhs1 = new Nilai();
+        mhs1.nim = "2310631170071";
+        mhs1.nama = "Desman Dwi Saputra";
+        mhs1.absen = 100;
+        mhs1.tugas = 95;
+        mhs1.uts = 95;
+        mhs1.uas = 90;
+        mhs1.Nilai();
+        mhs1.CetakNilai();
+    }
+}
